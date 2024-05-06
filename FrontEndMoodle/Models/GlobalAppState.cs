c826569ec8a3d01aa6e7fa2f065391b8997fc36f
binje <binje@BINJESUS>
2024-05-06 10:05:17 +0300

    register and sign in pages, models file with globalappstate, program.cs

---
+++ FrontEndMoodle/Models/GlobalAppState.cs
@@ -0,0 +1,34 @@
+﻿namespace FrontEndMoodle.Models
+{
+
+    public class GlobalAppState
+    {
+        // Property to store the authentication token
+        public string AuthToken { get; set; } = string.Empty;
+
+        // Property to store the username
+        public string Username { get; set; } = string.Empty;
+
+        // Property to store whether the user is an admin
+        public bool IsAdmin { get; set; } = false;
+
+        // Method to set authentication data
+        public void SetAuthData(string token, string username, bool isAdmin)
+        {
+            AuthToken = token;
+            Username = username;
+            IsAdmin = isAdmin;
+        }
+
+        // Method to clear all authentication data
+        public void ClearAuthData()
+        {
+            AuthToken = string.Empty;
+            Username = string.Empty;
+            IsAdmin = false;
+        }
+    }
+
+
+
+}
